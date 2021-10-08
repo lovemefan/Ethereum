df33b8b7b15c2fdaad9e8618fe982346a6c8a8e1
lovemefan <450489712@qq.com>
2021-10-08 14:29:00 +0800

    upload ResponseBody

---
+++ model/ResponseBody.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2020/12/25 下午9:49
+# @Author  : lovemefan
+# @File    : ResponseBody.py
+import json
+
+
+class ResponseBody:
+    """The response body of http"""
+
+    def __init__(self, message='', data='', code=200):
+        self.message = message
+        self.data = data
+        self.code = code
+
+    def to_json_string(self):
+        return json.loads(self.__dict__)
